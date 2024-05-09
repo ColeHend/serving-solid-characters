e5c8346391680a9c3e3e670f1727666a40b1c05d
Cole Henderson <darkgale93@gmail.com>
2024-05-09 08:30:19 -0600

    tab update

--- client/src/customHooks/utility/tabBar.ts
+++ client/src/customHooks/utility/tabBar.ts
@@ -8,30 +8,40 @@ export default function useTabs(pageName: Accessor<string>): Accessor<Tab[]> {
     const defaultSystem = GameSystems.DnD5e;
 
     effect(()=>{
-        if (pageName().startsWith('/info')) {
-            const theTabs = [
-                {Name: "Characters", Link: "/info/characters"}, 
-                {Name: "Races", Link: "/info/races"},
-                {Name: "Spells", Link: "/info/spells"},
-                {Name: "Feats", Link: "/info/feats"}
-            ];
-    
-            if (defaultSystem === GameSystems.DnD5e) {
-                theTabs.push({Name: "Backgrounds", Link: "/info/backgrounds"})
-                
-            } else if (defaultSystem === GameSystems.TOValient) {
-                theTabs.push({Name: "Heritages", Link: "/info/heritages"})
-                
-            }
-    
-            setTabPage(theTabs)
-        } else if (pageName().startsWith('/characters')) {
-            setTabPage([
-                {Name: "All", Link: "/characters"}, 
-                {Name: "Create", Link: "/characters/create"}
-            ])
-        } else {
-            setTabPage([])
+        switch (true) {
+            case pageName().startsWith('/info'):
+                const theTabs = [
+                    { Name: "Races", Link: "/info/races" },
+                    { Name: "Spells", Link: "/info/spells" },
+                    { Name: "Feats", Link: "/info/feats" }
+                ];
+
+                if (defaultSystem === GameSystems.DnD5e) {
+                    theTabs.push({ Name: "Backgrounds", Link: "/info/backgrounds" })
+
+                } else if (defaultSystem === GameSystems.TOValient) {
+                    theTabs.push({ Name: "Heritages", Link: "/info/heritages" })
+
+                }
+
+                setTabPage(theTabs);
+                break;
+
+            case pageName().startsWith('/characters'):
+                setTabPage([
+                    { Name: "All", Link: "/characters" },
+                    { Name: "View", Link: "/characters/view" },
+                    { Name: "Create", Link: "/characters/create" }
+                ]);
+                break;
+
+            case pageName().startsWith('/characters'):
+                // Add your code here for the condition case
+                break;
+
+            default:
+                setTabPage([]);
+                break;
         }
     })
 
