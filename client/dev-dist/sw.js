b0bc923c9fb000a85147e879a3ca1fd1350849d0
Cole Henderson <darkgale93@gmail.com>
2024-07-10 17:06:27 -0600

    latest built client

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.qsfr2a8qru"
+    "revision": "0.v1u2gi5fp2g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
