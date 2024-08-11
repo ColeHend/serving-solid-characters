fa98be665cc0d5c8f2a0e874c183c9273e35359b
Cole Henderson <darkgale93@gmail.com>
2024-08-11 11:23:14 -0600

    shared changes

--- client/src/components/homebrew/create/parts/spells/spells.tsx
+++ client/src/components/homebrew/create/parts/spells/spells.tsx
@@ -1,19 +1,17 @@
 import { Component, For, createSignal, useContext, createMemo } from "solid-js";
-import useStyle from "../../../../../shared/customHooks/utility/style/styleHook";
+import { useStyle, getUserSettings, Body } from "../../../../../shared/";
 import styles from './spells.module.scss'
 import type { Tab } from "../../../../navbar/navbar";
 import HomebrewSidebar from "../../sidebar";
 import { SharedHookContext } from "../../../../rootApp";
-import useStyles from "../../../../../shared/customHooks/utility/style/styleHook";
-import getUserSettings from "../../../../../shared/customHooks/userSettings";
 
 const Spells: Component = () => {
     const sharedHooks = useContext(SharedHookContext);
     const [userSettings, setUserSettings] = getUserSettings();
-    const stylin = createMemo(()=>useStyles(userSettings().theme));
+    const stylin = createMemo(()=>useStyle(userSettings().theme));
     return (
         <>
-            <div class={`${stylin()?.primary} ${styles.body}`}>
+            <Body>
                 <h1>spells</h1>
                 <p>name</p>
                 <p>level</p>
@@ -24,7 +22,7 @@ const Spells: Component = () => {
                 <p>duration</p>
                 <p>description</p>
                 <p>classes list</p>
-            </div>
+            </Body>
         </>
     );
 }
