d5d9ba4869b7aa96e0b644bc550ed3164c0ad81c
Slade <121606030+SladeAnderson@users.noreply.github.com>
2024-09-16 15:57:24 -0600

    whatever

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.pr21df36ksg"
+    "revision": "0.vrsap0173bo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
