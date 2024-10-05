3d840c8af3519bdf310562f9a6bbfc6c8177c751
Cole Henderson <darkgale93@gmail.com>
2024-10-04 22:50:16 -0600

    races start

--- client/src/components/homebrew/create/parts/races/races.tsx
+++ client/src/components/homebrew/create/parts/races/races.tsx
@@ -1,14 +1,36 @@
 import { Component, For, createSignal, useContext, createMemo } from "solid-js";
-import { useStyle, getUserSettings, Body } from "../../../../../shared/";
+import { useStyle, getUserSettings, Body, homebrewManager } from "../../../../../shared/";
 import styles from './races.module.scss'
 import type { Tab } from "../../../../navbar/navbar";
 import HomebrewSidebar from "../../sidebar";
 import { SharedHookContext } from "../../../../rootApp";
+import { Race } from "../../../../../models";
+import { createStore } from "solid-js/store";
+import { FeatureTypes } from "../../../../../models/core.model";
 
 const Races: Component = () => {
     const sharedHooks = useContext(SharedHookContext);
     const [userSettings, setUserSettings] = getUserSettings();
     const stylin = createMemo(()=>useStyle(userSettings().theme));
+		
+		const [currentRace, setCurrentRace] = createStore<Race>({
+			name: "",
+			speed: 30,
+			age: "",
+			alignment: "",
+			size: "",
+			sizeDescription: "",
+			languages: [],
+			languageChoice: {choose: 0, choices: [], type: FeatureTypes.Language},
+			languageDesc: "",
+			traits: [],
+			traitChoice: {choose: 0, choices: [], type: FeatureTypes.Race},
+			startingProficencies: [],
+			startingProficiencyChoices: {choose: 0, choices: [], type: FeatureTypes.Race},
+			abilityBonuses: [],
+			abilityBonusChoice: {choose: 0, choices: [], type: FeatureTypes.AbilityScore},
+			subRaces: []
+		});
     return (
         <>
             <Body>
