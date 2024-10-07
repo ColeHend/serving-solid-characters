c8cf6f8ede5cf67a8845559432b776776f17564d
Slade <121606030+SladeAnderson@users.noreply.github.com>
2024-10-07 16:17:15 -0600

    import idk

--- client/src/components/DataTransfering/Import/Importing.tsx
+++ client/src/components/DataTransfering/Import/Importing.tsx
@@ -1,11 +1,21 @@
-import { Component } from "solid-js";
-
-
+import { createDropzone, createFileUploader, UploadFile,fileUploader } from "@solid-primitives/upload";
+import { Component, createSignal } from "solid-js";
+import { importJsonObject } from "../../../shared/customHooks/utility/importJsonObject";
+import { Trade } from "../../../models/trade.model";
 
 const Importing:Component = () => {
-    
+    const [files,setFiles] = createSignal<UploadFile[]>([]);
 
-    return <></>
+
+    return <div>
+        <input
+        type="file"
+        multiple
+        use:fileUploader={{
+            userCallback: fs => fs.forEach(file => importJsonObject(file as unknown as Trade)),
+            setFiles,
+        }}/>
+    </div>
 }
 
 export default Importing;
