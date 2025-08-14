a4c442639a979ca92f3db560cd069ee1c06b5594
SladeAnderson <sladeanderson85@gmail.com>
2025-08-14 16:22:22 -0600

    Enhance CharacterView layout with additional info boxes and styling adjustments

--- client/src/components/characters/view/view.tsx
+++ client/src/components/characters/view/view.tsx
@@ -30,17 +30,62 @@ const CharacterView: Component = () => {
     <Body class={`${stylin().accent} ${styles.mainBody}`}>
       <h1>Characters View</h1>
       <div>
-        <div class={`${stylin().box}`}>
+        <div class={`${stylin().box} ${styles.baseInfoBox}`}>
           <Select value={currentCharacter()} onChange={(e)=>setCurrentCharacter(e)}>
             <For each={characters()}>{(character) => (
               <Option value={character}>{character.name}</Option>
             )}</For>
           </Select>
 
+          <div style={{display: "flex", "flex-direction": "row", "gap": "15px", "flex-wrap": "wrap",height:"19vh","margin-top":"2%"}}>
+            <div class={`${styles.infoBox}`}>
+              <Input transparent />
+              <hr />
+              <span>Background</span>
+            </div>
+
+            <div class={`${styles.infoBox}`}>
+              <Input transparent />
+              <hr />
+              <span>Class</span>
+            </div>
+
+
+            <div class={`${styles.infoBox}`}>
+              <Input transparent />
+              <hr />
+              <span>Speices</span>
+            </div>
+
+            <div class={`${styles.infoBox}`}>
+              <Input transparent />
+              <hr />
+              <span>Subclass</span>
+            </div>
+
+            <div class={`${styles.levelBox}`}>
+              <div class={`${styles.centerdInputBox}`}>
+                <Input transparent type="number" class={`${styles.levelInput}`} />
+                <hr class={`${styles.bigHR}`}/>
+                <span>Level</span>
+              </div>
+              <div class={`${styles.centerdInputBox}`}>
+                <Input transparent type="number" class={`${styles.expInput}`} />
+                <hr class={`${styles.smallHR}`}/>
+                <span>Exp</span>
+              </div>
+            </div>
+
+          </div>
+
+
+          
         </div>
         <div>
           <div>
-            <h2>Stats</h2>
+            {/* <h2>Stats</h2> */}
+            <hr class={`${styles.Divider}`}/>
+
             <StatBar fullStats={fullStats} currentCharacter={currentCharacter}/>
           </div>
         </div>
