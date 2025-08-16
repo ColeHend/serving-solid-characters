05c7677bb38da9370e1e3700f1ba5d1a6792a73d
Cole Henderson <darkgale93@gmail.com>
2025-08-15 21:58:35 -0600

    spell fix

--- models/entities/SpellEntity.cs
+++ models/entities/SpellEntity.cs
@@ -1,18 +1,30 @@
 
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace sharpAngleTemplate.models.entities
 {
   public class SpellEntity {
         public string Name { get; set; } = string.Empty;
+        
+        [JsonPropertyName("description")]
         public string Desc { get; set; } = string.Empty;
         public string Duration { get; set; } = string.Empty;
-        public bool Concentration { get; set; } = false;
         public string? Level { get; set; } = string.Empty;
         public string Range { get; set; } = string.Empty;
+
+        [JsonPropertyName("casting_time")]
+        public string CastingTime { get; set; } = string.Empty;
+
+        public string Components { get; set; } = string.Empty;
+
+        [JsonPropertyName("is_concentration")]
+        public bool Concentration { get; set; } = false;
+        [JsonPropertyName("is_ritual")]
         public bool Ritual { get; set; } = false;
         public string School { get; set; } = string.Empty;
-        public string CastingTime { get; set; } = string.Empty;
+        
+        [JsonPropertyName("damage_type")]
         public string DamageType { get; set; } = string.Empty;
         public string Page { get; set; } = string.Empty;
         public bool IsMaterial { get; set; } = false;
