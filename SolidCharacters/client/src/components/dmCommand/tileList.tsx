4072ce17dc2a5724c71e9a0be7f3d04da9914829
Cole Henderson <darkgale93@gmail.com>
2026-06-30 19:50:46 -0600

    update

--- SolidCharacters/client/src/components/dmCommand/tileList.tsx
+++ SolidCharacters/client/src/components/dmCommand/tileList.tsx
@@ -3,14 +3,14 @@ import { JSX } from "solid-js";
 /**
  * The Width is columns and the height is rows. So a tile that is 2x3 would be 2 columns wide and 3 rows tall.
  */
-interface TileSize {
+interface TileData {
     width: number;
     height: number;
 }
 
 export type tiles = 'combatTracker';
 
-export const tileSizes: Record<tiles, TileSize> = {
+export const tileSizes: Record<tiles, TileData> = {
     combatTracker: { width: 2, height: 3 },
 };
 export default function GetTileElement(tile: tiles): JSX.Element {
