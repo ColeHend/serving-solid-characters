a54be17fd896e83d8f0e2f291e6d7a6ac5caf2ac
Cole Henderson <darkgale93@gmail.com>
2026-07-14 11:03:28 -0600

    configurable

--- SolidCharacters/client/src/shared/customHooks/utility/tools/getScreenSize.ts
+++ SolidCharacters/client/src/shared/customHooks/utility/tools/getScreenSize.ts
@@ -4,21 +4,25 @@ type ScreenSize = 'small' | 'medium' | 'large';
 interface ScreenSizeInfo {
     screenSize: Accessor<ScreenSize>;
 }
+interface ScreenSizeThresholds {
+    small: number;
+    medium: number;
+}
 /**
  * New utility hook to determine if is mobile
  * Should be used in place of isMobile() from coles-solid-library
  * Since isMobile() is not reactive and does not update on window resize
  * @returns An live updating signal with how wide the screen is.
  */
-export const getScreenSize = (): ScreenSizeInfo  => {
+export const getScreenSize = (customSizes?: ScreenSizeThresholds): ScreenSizeInfo  => {
     const [currScreenSize, setCurrScreenSize] = createSignal<ScreenSize>('large');
     const screenSize = createMemo(() => currScreenSize());
     
     const updateScreenSize = () => {
         const width = window.innerWidth;
-        if (width < 600) {
+        if (width < (customSizes?.small ?? 600)) {
             setCurrScreenSize('small');
-        } else if (width < 1024) {
+        } else if (width < (customSizes?.medium ?? 1024)) {
             setCurrScreenSize('medium');
         } else {
             setCurrScreenSize('large');
